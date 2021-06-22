79dd8470c291d49a623eddfdf82295b3179e221f
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-06-22 17:29:53 -0400

    add cors for all requests

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
