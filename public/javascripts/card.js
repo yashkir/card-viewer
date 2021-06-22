10dffd7e56bf1960e834528eaeb6fd3aeca54fe5
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-06-22 17:26:32 -0400

    Implement card selection form

--- public/javascripts/card.js
+++ public/javascripts/card.js
@@ -1,15 +1,25 @@
 var imageEl = document.getElementById('image');
-var cardEl = document.getElementById('card');
 var avatarEl = document.getElementById('avatar');
 var contentEl = document.getElementById('content');
-var followBtnEl = document.getElementById('follow-btn');
+var setCardBtnEl = document.getElementById('set-card-btn');
+var cardNumEl = document.getElementById('card-num');
+var currentCardNumEl = document.getElementById('current-card-num');
 
-var cardNum = 1;
+function loadCard(cardNum) {
+  fetch("http://localhost:3000/api/cards/" + cardNum)
+    .then(card => card.json())
+    .then(content => {
+      imageEl.setAttribute("src", content.image.url); 
+      avatarEl.setAttribute("src", content.avatar.url);
+      contentEl.innerHTML = `<h4>${content.post.title}</h4><p>${content.post.body}</p>`;
+      currentCardNumEl.innerHTML = `${cardNum}`;
+    });
 
-fetch("http://localhost:3000/api/cards/" + cardNum)
-  .then(card => card.json())
-  .then(content => {
-    imageEl.setAttribute("src", content.image.url); 
-    avatarEl.setAttribute("src", content.avatar.url);
-    contentEl.innerHTML = `<h4>${content.post.title}</h4><p>${content.post.body}</p>`;
-  });
+}
+
+setCardBtnEl.addEventListener("click", e => {
+  e.preventDefault();
+  loadCard(cardNumEl.value);
+});
+
+loadCard(cardNumEl.value);
